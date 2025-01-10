71d50a4c04546b398a1f38f69f883a291e08f633
klx <k.l.x.venneman@student.nl>
2025-01-10 13:51:03 +0100

    BERT change structure

--- BERT_transformer.py
+++ BERT_transformer.py
@@ -13,22 +13,6 @@ from datetime import datetime
 from sklearn.metrics import (classification_report, accuracy_score, f1_score,
                              precision_score, recall_score, confusion_matrix)
 
-# Callback function to log the evaluation losses for each training epoch
-class LoggingCallback(TrainerCallback):
-    def on_log(self, args, state: TrainerState, control: TrainerControl, **kwargs):
-        logging.info(f"Step: {state.global_step}, Loss: {state.log_history[-1].get('loss')}")
-        for key, value in state.log_history[-1].items():
-            logging.info(f"{key}: {value}")
-
-        # Add a whitespace line after the complete logging event
-        logging.info("\n")
-
-# Create function for printing
-def print_custom(text):
-    print('\n')
-    print(text)
-    print('-'*100)
-
 # Create callback function to log Optuna trial information
 def print_trial_info(study, trial):
     logging.info(f"Trial {trial.number}:")
@@ -40,6 +24,27 @@ def print_trial_info(study, trial):
 
     logging.info("\n")
 
+# Callback function to log the evaluation losses for each training epoch
+class LoggingCallback(TrainerCallback):
+    def on_log(self, args, state: TrainerState, control: TrainerControl, **kwargs):
+        logging.info(f"Step: {state.global_step}, Loss: {state.log_history[-1].get('loss')}")
+        for key, value in state.log_history[-1].items():
+            logging.info(f"{key}: {value}")
+
+        # Add a whitespace line after the complete logging event
+        logging.info("\n")
+
+# Tokenize text in df with truncation
+def encode(dataset):
+    if debiased_dataset:
+        outputs = tokenizer(
+            dataset['debiased_post'], truncation=True, padding='max_length',
+                max_length=512)
+    else:
+        outputs = tokenizer(
+            dataset['processed_post'], truncation=True, padding='max_length',
+            max_length=512)
+    return outputs
 
 def objective(trial: optuna.Trial):
     """
@@ -78,30 +83,23 @@ def objective(trial: optuna.Trial):
 
     return eval_result['eval_loss']     # eval loss
 
-
-# Tokenize text in df with truncation
-def encode(dataset):
-    outputs = tokenizer(
-        dataset['processed_post'], truncation=True, padding='max_length',
-            max_length=512)
-    return outputs
-
 if __name__ == '__main__':
     # CONSTANTS
-    train_path = "./data/train_debiased_1000.csv"
-    val_path = "./data/val_debiased_1000.csv"
-    test_path = "./data/test_debiased_1000.csv"
-    model_variant = "bert-small"     # tiny, mini, small, medium
-    num_trials = 3
+    debiased_dataset = True  # to determine which column of text the model will use; True for debiased_post
+    train_path = "./data/train_debiased_500.csv"
+    val_path = "./data/val_debiased_500.csv"
+    test_path = "./data/test_debiased_500.csv"
+    model_variant = "bert-mini"  # tiny, mini, small, medium
+    num_trials = 12
 
     # Configure logging
     if not path.exists('logging'):
         os.mkdir('logging')
     current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    logging.basicConfig(filename=f"./logging/optuna_logs_{current_time}.log", level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+    logging.basicConfig(filename=f"./logging/optuna_logs_{current_time}.log", level=logging.INFO,
+                        format="%(asctime)s - %(levelname)s - %(message)s")
 
-
-
+    # Import data from csv's
     train_df = pd.read_csv(train_path)
     val_df = pd.read_csv(val_path)
     test_df = pd.read_csv(test_path)
@@ -120,45 +118,38 @@ if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
 
-
-
+    # Apply the tokenization function to all datasets and rename the label columns for expected format
     train_dataset = train_dataset.map(encode, batched=True)
+    train_dataset = train_dataset.rename_column('label', 'labels')
     evaluation_dataset = evaluation_dataset.map(encode, batched=True)
+    evaluation_dataset = evaluation_dataset.rename_column('label', 'labels')
     test_dataset = test_dataset.map(encode, batched=True)
+    test_dataset = test_dataset.rename_column('label', 'labels')
 
     # Data collator to dynamically pad sequences in each batch
     data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
-
-    #----------------------------------------------------------------------------------------------------
+    # ----------------------------------------------------------------------------------------------------
     #                    CREATE OPTUNA STUDY
-    #----------------------------------------------------------------------------------------------------
+    # ----------------------------------------------------------------------------------------------------
 
     logging.info("STARTING HYPERPARAMETER TUNING")
     logging.info("-----------------------------------\n")
 
-    print_custom('Triggering Optuna study')
-    study = optuna.create_study(study_name='hp-search-electra', direction='minimize')    # minimize evaluation loss
-    study.optimize(func=objective, n_trials=num_trials, callbacks=[print_trial_info])    # callback for logging trials
+    study = optuna.create_study(study_name='hp-search-electra', direction='minimize')  # minimize evaluation loss
+    study.optimize(func=objective, n_trials=num_trials, callbacks=[print_trial_info])  # callback for logging trials
 
     logging.info("FINISHED HYPERPARAMETER TUNING")
     logging.info("-----------------------------------\n")
 
-    #----------------------------------------------------------------------------------------------------
+    # ----------------------------------------------------------------------------------------------------
     #                    PRINT BEST STUDY HYPERPARAMETERS
-    #----------------------------------------------------------------------------------------------------
+    # ----------------------------------------------------------------------------------------------------
 
-    print_custom('Finding study best parameters')
     best_lr = float(study.best_params['learning_rate'])
     best_weight_decay = float(study.best_params['weight_decay'])
     best_epoch = int(study.best_params['num_train_epochs'])
     best_batch_size = int(study.best_params['per_device_train_batch_size'])
-
-    print_custom('Extract best study params')
-    print(f'The best learning rate is: {best_lr}')
-    print(f'The best weight decay is: {best_weight_decay}')
-    print(f'The best epoch is : {best_epoch}')
-    print(f'The best batch size is : {best_batch_size}')
 
     logging.info("EXTRACTING BEST PARAMETERS")
     logging.info(f"Best Learning Rate: {best_lr}")
@@ -173,8 +164,6 @@ if __name__ == '__main__':
 
     logging.info("TRAINING MODEL WITH BEST PARAMETERS")
     logging.info("-----------------------------------\n")
-
-    print_custom('Training the model on the custom parameters')
 
     training_args = TrainingArguments(
         output_dir='./results',
@@ -212,7 +201,6 @@ if __name__ == '__main__':
     logging.info("SAVING BEST TUNED MODEL")
     logging.info("-----------------------------------\n")
 
-    print_custom('Saving the best Optuna tuned model')
     if not path.exists('model'):
         os.mkdir('model')
 
@@ -224,7 +212,7 @@ if __name__ == '__main__':
     #                   TRAINED MODEL METRICS
     # ----------------------------------------------------------------------------------------------------
 
-    # Temporarily changing logging format
+    # Changing logging format (Does not use "Time - Info - Text" format)
     formatter = logging.Formatter("%(message)s")
     for handler in logging.getLogger().handlers:
         handler.setFormatter(formatter)
@@ -237,23 +225,20 @@ if __name__ == '__main__':
     predicted_labels = predictions.predictions.argmax(-1)
 
     # Get actual labels from unseen test dataset
-    actual_labels = [example['label'] for example in test_dataset]
+    actual_labels = [example['labels'] for example in test_dataset]
 
     # Calculate metrics
     accuracy = accuracy_score(actual_labels, predicted_labels)
     f1 = f1_score(actual_labels, predicted_labels, average='weighted')
     precision = precision_score(actual_labels, predicted_labels, average='weighted')
     recall = recall_score(actual_labels, predicted_labels, average='weighted')
-    class_report = classification_report(actual_labels, predicted_labels, target_names=["0", "1", "2"])     # Summary of above
+    class_report = classification_report(actual_labels, predicted_labels,
+                                         target_names=["0", "1", "2"])  # Summary of above
 
     # Confusion matrix
     conf_matrix = confusion_matrix(actual_labels, predicted_labels)
     conf_matrix_df = pd.DataFrame(conf_matrix, index=["Actual 0", "Actual 1", "Actual 2"],
                                   columns=["Pred 0", "Pred 1", "Pred 2"])
-
-    # Logging metrics
-    logging.info("FINISHED EVALUATION")
-    logging.info("-----------------------------------\n")
 
     logging.info(f"Accuracy: {accuracy}")
     logging.info(f"F1 Score: {f1}")
@@ -263,3 +248,7 @@ if __name__ == '__main__':
     logging.info(class_report)
     logging.info("\nConfusion Matrix:\n")
     logging.info(conf_matrix_df)
+
+    # Logging metrics
+    logging.info("FINISHED EVALUATION")
+    logging.info("-----------------------------------\n")
